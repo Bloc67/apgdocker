e1d542e78e5a7b5bd9430b5670f4becfd3b2a257
Bjørn Kristiansen <post@bhkristiansen.com>
2025-06-27 13:27:41 +0200

    ny

--- src/index.php
+++ src/index.php
@@ -5,17 +5,17 @@ echo '
 <html lang="en">
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="http://127.0.0.1/adcomhub/ext/site.css?v=1">
+	<link rel="stylesheet" href="http://127.0.0.1/apg/ext/site.css?v=1">
 	<meta charset="UTF-8">
 <link rel="apple-touch-icon" sizes="180x180" href="http://127.0.0.1/adcomhub/ext/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="http://127.0.0.1/adcomhub/ext/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="http://127.0.0.1/adcomhub/ext/favicon-16x16.png">
-<link rel="manifest" href="http://127.0.0.1/adcomhub/ext/site.webmanifest">
+<link rel="manifest" href="http://127.0.0.1/apg/ext/site.webmanifest">
 	<title>AdcomHub - Admin</title>
 </head>
 <body>
 	<header id="h1">
-		<h1>Oversikt</h1>
+		<h1>Oversikt localhost</h1>
 	</header>
 	<div class="content">   
     <nav>
